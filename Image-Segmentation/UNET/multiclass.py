2bd25aac756e706b8e08fbd41f1811b67c207256
Jaisidh Singh <75247817+jaisidhsingh@users.noreply.github.com>
2021-05-23 12:46:26 +0530

    Update multiclass.py

--- Image-Segmentation/UNET/multiclass.py
+++ Image-Segmentation/UNET/multiclass.py
@@ -36,10 +36,10 @@ def decode_segmap(image, nc=21):
   	b = np.zeros_like(image).astype(np.uint8)
 
   	for l in range(0, nc):
-    	idx = image == l
-    	r[idx] = label_colors[l, 0]
-    	g[idx] = label_colors[l, 1]
-    	b[idx] = label_colors[l, 2]
+    		idx = image == l
+    		r[idx] = label_colors[l, 0]
+    		g[idx] = label_colors[l, 1]
+    		b[idx] = label_colors[l, 2]
 
   	rgb = np.stack([r, g, b], axis=2)
   	return rgb
